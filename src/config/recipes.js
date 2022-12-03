662ef8287be70ab8190ced0d2a79c3389c1814bf
hainam1702 <112164979+hainam1702@users.noreply.github.com>
2022-12-03 12:13:58 +0700

    first commit

---
+++ src/config/recipes.js
@@ -0,0 +1,84 @@
+const recipes = [
+    {
+        id: 1,
+        name: "Áo khoác gió Hàn Quốc",
+        price: 80,
+        cate: 'Áo khoác',
+        image: require('../../assets/pngegg.png'),
+        description:
+            "Ai cũng mong muốn tìm cho mình một chiếc 'ÁO KHOÁC' thật đẹp và phù hợp với mình, bởi thời đại 4.0 thì áo khoác ngoài việc bảo vệ thì nó còn là vấn đề về thời trang, không những vậy còn phải đáp ứng được những yêu cầu khác như:Không gây khó chịu nếu mặc vào mùa nắng nóng khi di chuyển ngoài đường ,khả năng thoát mồ hôi thấm hút mồ hôi và không để lại mùi hôi nếu mặc áo trong khoảng thời gian  dài ,có thể sử dụng tốt vào mùa mưa ... Và chiếc 'ÁO KHOÁC' với chất liệu Vải dù mero từ Huy Nam Shop chính là giải pháp dành cho bạn, một chiếc 'ÁO KHOÁC' thời trang và giúp bảo vệ bạn với chất liệu Vải dù 99% polyester, 1% spandex(vải mero) mang đến trải nghiệm “cực kỳ thoải mái” và không gây khó chịu như những sản phẩm 'ÁO KHOÁC' bạn đã từng trải nghiệm trước đây.",
+        rating: 4.7,
+    },
+    {
+        id: 2,
+        name: "Black mamba",
+        price: 55,
+        cate: 'Headphones',
+        image: require('../../assets/headphones-2.png'),
+        description:
+            "Minim id eiusmod laborum excepteur officia. Lorem ad enim voluptate elit sunt velit nostrud voluptate. Ut nostrud et aliqua do eiusmod laboris magna occaecat aliquip. Reprehenderit veniam veniam cupidatat labore dolore aute laboris voluptate consectetur consectetur elit.",
+        rating: 4.7,
+    },
+    {
+        id: 3,
+        name: "Red Onnion",
+        price: 55,
+        cate: 'Headphones',
+        image: require('../../assets/headphones-3.png'),
+        description:
+            "Minim id eiusmod laborum excepteur officia. Lorem ad enim voluptate elit sunt velit nostrud voluptate. Ut nostrud et aliqua do eiusmod laboris magna occaecat aliquip. Reprehenderit veniam veniam cupidatat labore dolore aute laboris voluptate consectetur consectetur elit.",
+        rating: 4.7,
+    },
+    {
+        id: 4,
+        name: "Black mamba",
+        price: 55,
+        cate: 'Headphones',
+        image: require('../../assets/headphones-4.png'),
+        description:
+            "Minim id eiusmod laborum excepteur officia. Lorem ad enim voluptate elit sunt velit nostrud voluptate. Ut nostrud et aliqua do eiusmod laboris magna occaecat aliquip. Reprehenderit veniam veniam cupidatat labore dolore aute laboris voluptate consectetur consectetur elit.",
+        rating: 4.7,
+    },
+    {
+        id: 5,
+        name: "Red Onnion",
+        price: 55,
+        cate: 'Headphones',
+        image: require('../../assets/headphones-4.png'),
+        description:
+            "Minim id eiusmod laborum excepteur officia. Lorem ad enim voluptate elit sunt velit nostrud voluptate. Ut nostrud et aliqua do eiusmod laboris magna occaecat aliquip. Reprehenderit veniam veniam cupidatat labore dolore aute laboris voluptate consectetur consectetur elit.",
+        rating: 4.7,
+    },
+    {
+        id: 6,
+        name: "Black mamba",
+        price: 55,
+        cate: 'Headphones',
+        image: require('../../assets/headphones-3.png'),
+        description:
+            "Minim id eiusmod laborum excepteur officia. Lorem ad enim voluptate elit sunt velit nostrud voluptate. Ut nostrud et aliqua do eiusmod laboris magna occaecat aliquip. Reprehenderit veniam veniam cupidatat labore dolore aute laboris voluptate consectetur consectetur elit.",
+        rating: 4.7,
+    },
+    {
+        id: 7,
+        name: "Red Onnion",
+        price: 55,
+        cate: 'Headphones',
+        image: require('../../assets/headphones-2.png'),
+        description:
+            "Minim id eiusmod laborum excepteur officia. Lorem ad enim voluptate elit sunt velit nostrud voluptate. Ut nostrud et aliqua do eiusmod laboris magna occaecat aliquip. Reprehenderit veniam veniam cupidatat labore dolore aute laboris voluptate consectetur consectetur elit.",
+        rating: 4.7,
+    },
+    {
+        id: 8,
+        name: "Black mamba",
+        price: 55,
+        cate: 'Headphones',
+        image: require('../../assets/headphones-1.png'),
+        description:
+            "Minim id eiusmod laborum excepteur officia. Lorem ad enim voluptate elit sunt velit nostrud voluptate. Ut nostrud et aliqua do eiusmod laboris magna occaecat aliquip. Reprehenderit veniam veniam cupidatat labore dolore aute laboris voluptate consectetur consectetur elit.",
+        rating: 4.7,
+    },
+];
+
+export default recipes;
